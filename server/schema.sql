0d0a61de10f547622b11b5585dcb28d393e032fa
evanwchen <evanwchen@gmail.com>
2016-02-05 20:04:22 -0800

    Set up tables in our mySQL db.

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,28 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+CREATE TABLE users (id INT NOT NULL, 
+  username VARCHAR(50) NOT NULL,  
+  PRIMARY KEY (id));
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE chatrooms (id INT NOT NULL, 
+  roomname VARCHAR(50) NOT NULL,  
+  PRIMARY KEY (id));
 
+CREATE TABLE messages (id INT NOT NULL, 
+  message VARCHAR(200) NOT NULL, 
+  chatroomID INT NOT NULL, 
+  userID INT(4) NOT NULL, 
+  PRIMARY KEY (id),
+  FOREIGN KEY (chatroomID) references chatrooms (id),
+  FOREIGN KEY (userID) references users (id));
 
+CREATE TABLE friends (id INT NOT NULL, 
+  userID INT(4) NOT NULL,
+  friendID INT(4) NOT NULL,    
+  PRIMARY KEY (id),
+  FOREIGN KEY (userID) references users (id),
+  FOREIGN KEY (friendID) references users (id));
 
 
 /*  Execute this file from the command line by typing:
